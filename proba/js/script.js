9b3306ad31974c1feb5beb513b3f314a1ec0cc42
Yanior <perelozhilov@yandex.ru>
2019-02-13 13:48:37 +0300

    add

--- proba/js/script.js
+++ proba/js/script.js
@@ -2,21 +2,20 @@ window.onload = function () {
     var wrap = document.getElementById('popup_overlay');
     var closeB = document.getElementById('popup_close');
     var btn = document.getElementById('popupIn');
-    btn.onclick = popupOpen;
-    closeB.onclick = popupClose;
 
     function popupOpen() {
         wrap.style.display = 'block';
         popupBtn(1);
     }
+    btn.onclick = popupOpen;
 
     function popupBtn(x) {
         var op = (wrap.style.opacity) ? parseFloat(wrap.style.opacity) : 0;
-        
-        if(op < x ) {
+
+        if (op < x) {
             op += 0.05;
             wrap.style.opacity = op;
-            
+
 
             setTimeout(function () {
                 popupBtn(x);
@@ -24,44 +23,80 @@ window.onload = function () {
         }
     }
 
-    function popup() { 
+    function popup() {
         wrap.style.display = 'block';
         popupIn(1);
-     }
+    }
 
-     function popupClose() {
-         popupOut(0);
-     }
+    function popupClose() {
+        popupOut(0);
+    }
+    closeB.onclick = popupClose;
 
-     function popupIn(x) {
+
+    var tIn, tOut;
+    function popupIn(x) {
         var op = (wrap.style.opacity) ? parseFloat(wrap.style.opacity) : 0;
 
-        if(op < x) {
+        if (op < x) {
+
+            clearInterval(tOut);
             op += 0.05;
             wrap.style.opacity = op;
 
             // setTimeout(popupIn, 50, x);
-            setTimeout(function () {
+            tIn = setTimeout(function () {
                 popupIn(x);
-            }, 50);
+            }, 50); 
         }
-     }
-     function popupOut(x) {
+    }
+
+    function popupOut(x) {
         var op = (wrap.style.opacity) ? parseFloat(wrap.style.opacity) : 0;
 
-        if(op > x) {
+        if (op > x) {
+
+            clearInterval(tIn);
             op -= 0.05;
             wrap.style.opacity = op;
 
             // setTimeout(popupIn, 50, x);
-            setTimeout(function () {
+            tOut = setTimeout(function () {
                 popupOut(x);
             }, 50);
         }
-        if(wrap.style.opacity == x) {
-            wrap.style.display = 'none'; 
-         }
-     }
+        if (wrap.style.opacity == x) {
+            wrap.style.display = 'none';
+        }
+    }
 
-     setTimeout(popup, 3000);
-}
+    setTimeout(popup, 3000);
+
+    var h1 = document.getElementById('header');
+    h1.onclick = function () {
+        clearTimeout(intStop);
+    }
+
+    function changeColor() {
+        if(h1.style.color == 'black') {
+            h1.style.color = 'white';
+        } else {
+            h1.style.color = 'black';
+        }
+    }
+    function trySmt() {
+        var intStop = setInterval(changeColor, 500);
+
+        do {
+            var str = prompt('Введите сообщение');
+            var result = confirm('Вы ввели строку ' + str + '. Нажмите на OK для продолжения, или CANCEL для отмены');
+        } while (!result);
+        
+        if(str == "Да") {
+            alert('Ура')
+        } else {
+            alert('Неправильный ответ')
+        }
+    }
+    trySmt();
+}   
